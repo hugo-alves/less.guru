1d14810aeae0bd8cbc0661047840ff60ace859cc
Hugo Alves <goninja@gmail.com>
2016-05-06 14:06:19 +0100

    com novo look

--- functions.js
+++ functions.js
@@ -1,26 +1,45 @@
 var boxPrice;
 var cigarsPerDay;
 var age;
-var gender;
+var gender = "female";
 var pricePerDay;
 var pricePerYear;
-var reduceBy;
+var reduceBy = 1;
 var saveCigar;
 var saveLife;
 var savePerLife;
 var saveYear;
 var savePerYear;
+$(document).ready(function() {
+  if(gender == "female"){
+    $('.female').addClass('active');
+    $('.male').removeClass('active');
+  }
+});
+
+function getGender(value){
+  gender = value;
+  if(gender == "female"){
+    $('.female').addClass('active');
+    $('.male').removeClass('active');
+  }
+  else{
+    $('.female').removeClass('active');
+    $('.male').addClass('active');
+  }
+}
 
 function calculate(){
+
   var boxPrice = document.getElementById('boxPrice').value;
   var cigarsPerDay = document.getElementById('cigarsPerDay').value;
   var age = document.getElementById('age').value;
-  var gender = document.getElementById('gender').value;
   var reduceBy = document.getElementById('reduceBy').value;
+
   // reduce
     var pricePerDay = pricePerCigar * cigarsPerDay;
     var saveCigar = (pricePerCigar * (cigarsPerDay - reduceBy));
-    var pricePerCigar = ((boxPrice * 100) / 20 ) / 100;
+    var pricePerCigar = Math.round((((boxPrice * 100) / 20 ) / 100) * 100) / 100;
     var pricePerYear =  pricePerCigar * cigarsPerDay * 365;
     var saveYear = pricePerCigar * (cigarsPerDay - reduceBy) * 365;
     var savePerYear = pricePerYear - saveYear;
@@ -37,12 +56,16 @@ function calculate(){
       $('#year').animateNumber({ number: pricePerYear });
       $('#life').animateNumber({number: pricePerLife});
 
-      document.getElementById('form').style.display = 'none';
+      document.getElementById('inputs-container').style.display = 'none';
       document.getElementById('results').style.display = 'flex';
+      document.getElementById('reduceContainer').style.display = 'flex';
       document.getElementById('day').innerHTML = pricePerCigar +"€";
+      savings();
 
 };
 function recalculate(){
+  document.getElementById('inputs-container').style.display = 'block';
+  document.getElementById('reduceContainer').style.display = 'none';
   document.getElementById('form').style.display = 'block';
   document.getElementById('results').style.display = 'none';
 };
@@ -53,7 +76,6 @@ function savings(){
     var boxPrice = document.getElementById('boxPrice').value;
     var cigarsPerDay = document.getElementById('cigarsPerDay').value;
     var age = document.getElementById('age').value;
-    var gender = document.getElementById('gender').value;
     var reduceBy = document.getElementById('reduceBy').value;
       var pricePerDay = pricePerCigar * cigarsPerDay;
       var saveCigar = (pricePerCigar * (cigarsPerDay - reduceBy));
